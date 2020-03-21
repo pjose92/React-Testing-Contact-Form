ea33ae477b53398e858f44f3b09c88a9da323e76
Jose <pjose14.jp38@yahoo.com>
2020-03-21 16:36:38 -0500

    running two tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
-test("renders App without crashing", () => {
+test("Renders App without crashing", () => {
   render(<App />);
 });
 
+test("Rendering form inputs", () => {
+  const { getByLabelText } = render(<ContactForm />);
+  getByLabelText(/first name/i);
+  getByLabelText(/last name/i);
+  getByLabelText(/email/i);
+  getByLabelText(/message/i);
+});
+
+test("Validating inputs", () => {
+  const { getByLabelText } = render(<ContactForm />);
+  const firstNameInput = getByLabelText(/first name/i);
+  const lastNameInput = getByLabelText(/last name/i);
+  const emailInput = getByLabelText(/email/i);
+  const messageInput = getByLabelText(/message/i);
+
+  expect(firstNameInput).toHaveAttribute("type");
+  expect(lastNameInput).toHaveAttribute("type");
+  expect(emailInput).toHaveAttribute("type");
+  expect(messageInput).toHaveAttribute("type");
+})
